5776bad14bd92241854c819c60b066934282ead4
jeppacha <jepers.jorgep@gmail.com>
2022-04-05 11:19:51 -0300

    Arreglo de Footer

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	
-	<strong>Copyright &copy; 2021 <a href="https://www.camionera.com.ar" target="_blank">  Camionera Mendocina S.R.L. </a>
+	<strong>Copyright JORGE E. PEREZ &copy; 2021 <a href="https://www.camionera.com.ar" target="_blank">  Camionera Mendocina S.R.L. </a>
 
 	</strong>
 
